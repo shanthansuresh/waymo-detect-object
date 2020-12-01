fd46fa2b037cc8a00a07da4a673b5e30e3021289
sarada <shanthan.n2@gmail.com>
2020-12-01 22:36:09 +0100

    Split into train, val and test data
    Signed-off-by: shanthansuresh <shanthan.n2@gmail.com>

--- create_splits.py
+++ create_splits.py
@@ -2,6 +2,7 @@ import argparse
 import glob
 import os
 import random
+import shutil
 
 import numpy as np
 
@@ -17,6 +18,53 @@ def split(data_dir):
         - data_dir [str]: data directory, /mnt/data
     """
     # TODO: Implement function
+   
+    # Create train, val and test directories
+    train_dir = data_dir + "/train"
+    dir_exists = os.path.isdir(train_dir)
+    if (dir_exists):
+      shutil.rmtree(train_dir)
+
+    val_dir = data_dir + "/val"
+    dir_exists = os.path.isdir(val_dir)
+    if (dir_exists):
+      shutil.rmtree(val_dir)
+
+    test_dir = data_dir + "/test"
+    dir_exists = os.path.isdir(test_dir)
+    if (dir_exists):
+      shutil.rmtree(test_dir)
+
+    tfrec_files_list = os.listdir(data_dir)
+    num_tfrecs = len(tfrec_files_list)
+    random.shuffle(tfrec_files_list)
+
+    #Split in the ratio of 0.70 : 0.15 : 0.15 for the training, validation and test set
+    train_size = int(num_tfrecs * 0.70)
+    val_size   = int(num_tfrecs * 0.15)
+    test_size  = int(num_tfrecs * 0.15)
+
+    os.mkdir(train_dir)
+    os.mkdir(val_dir)
+    os.mkdir(test_dir)
+ 
+    dest_dir = train_dir
+    for i in range(train_size):
+      #Copy training data
+      src_file = data_dir + "/" + tfrec_files_list[i]
+      shutil.copy2(src_file, dest_dir)
+
+    dest_dir = val_dir
+    for i in range(train_size, (train_size+val_size)):
+        #Copy val data
+        src_file = data_dir + "/" + tfrec_files_list[i]
+        shutil.copy2(src_file, dest_dir)
+
+    dest_dir = test_dir
+    for i in range((train_size+val_size), (train_size+val_size+test_size)):
+        #Copy test data
+        src_file = data_dir + "/" + tfrec_files_list[i]
+        shutil.copy2(src_file, dest_dir)
     
 
 if __name__ == "__main__": 
